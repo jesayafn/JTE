461fdda8ea0465c25640bfb27c939ddf42e05b9a
Jesaya Fernando Napitupulu <52324282+jesayafn@users.noreply.github.com>
2023-02-20 19:18:12 +0700

    asas

--- messages/steps/print.groovy
+++ messages/steps/print.groovy
@@ -1,6 +1,6 @@
 void call(){
     stage("Messaging to the my world!")
-    String nodeName = config.messsages ?: "jesaya"
+    String nodeName = config.nodename ?: "jesaya"
     node(nodeName){
         println nodeName+" on WORK!"
     }
