07e1df225084910a79acfef8e62229ee41acb85c
Jesaya Fernando Napitupulu <52324282+jesayafn@users.noreply.github.com>
2023-02-20 19:24:47 +0700

    sas

--- messages/steps/print.groovy
+++ messages/steps/print.groovy
@@ -1,6 +1,6 @@
 void call(){
     stage("Messaging to the my world!")
-    String nodeName = config.nodename
+    String nodeName = config.nodename ?: "master"
     node(nodeName){
         println nodeName+" on WORK!"
     }
