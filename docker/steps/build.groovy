ed48643e812bbf3410330fdcfc32cce1df3591c5
Jesaya Fernando Napitupulu <52324282+jesayafn@users.noreply.github.com>
2023-02-21 12:16:27 +0700

    debug, disable image tag

--- docker/steps/build.groovy
+++ docker/steps/build.groovy
@@ -2,7 +2,7 @@ void call(app_env){
     String nodeName = config.nodename ?: "master"
     String userName = app_env.user
     String password = app_env.password
-    String imageTag = app_env.imageTag
+    // String imageTag = app_env.imageTag
     // if(!userName?.trim() || !password.trim() || !imageTag.trim()){
     //     println("ABORT! NEED SOME PARAMETER")  
     // } else{
