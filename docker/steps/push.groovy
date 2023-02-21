f55daded61e7c7e9c9ba41d8391cff93c2b6f61b
Jesaya Fernando Napitupulu <52324282+jesayafn@users.noreply.github.com>
2023-02-21 17:05:23 +0700

    fix variable on push.groovy

--- docker/steps/push.groovy
+++ docker/steps/push.groovy
@@ -4,6 +4,7 @@ void call(app_env){
     String userName = app_env.docker_user
     String password = app_env.docker_password
     String registry = app_env.docker_registry
+    String tag = app_env.docker_image_tag
     stage("Build Image"){
         node(nodeName){
         sh """docker login https://${registry} --username ${userName} --password ${password}"""
