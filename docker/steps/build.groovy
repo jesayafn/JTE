39838091b4e1b66289f0e66617fa872f941e0c41
Jesaya Fernando Napitupulu <52324282+jesayafn@users.noreply.github.com>
2023-02-21 12:05:49 +0700

    add docker

---
+++ docker/steps/build.groovy
@@ -0,0 +1,15 @@
+void call(app_env){
+    String nodeName = config.nodename ?: "master"
+    String userName = app_env.user
+    String password = app_env.password
+    String imageTag = app_env.imageTag
+    if(userName=null || password=null || imageTag=null){
+        println("ABORT! NEED SOME PARAMETER")  
+    } else{
+        node(nodeName){
+        stage("Build Image"){
+            sh '''buiildah login --username ${app_env.user} --password ${app_env.password --verbose'''
+        }
+    }
+    }
+}
