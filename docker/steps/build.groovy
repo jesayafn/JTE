093750512f4ad388906d281bb98580b19531b381
Jesaya Fernando Napitupulu <52324282+jesayafn@users.noreply.github.com>
2023-02-21 17:00:10 +0700

    fix

--- docker/steps/build.groovy
+++ docker/steps/build.groovy
@@ -1,14 +1,14 @@
 void call(app_env){
     String dirContext = app_env.dir_context
     String nodeName = config.nodename ?: "master"
-    String userName = app_env.user
+    String userName = app_env.docker_user
     String tag = app_env.docker_image_tag
     // String password = app_env.password
-    String registryUrl = app_env.docker_url
+    String registry = app_env.docker_registry
     stage("Build Image"){
         node(nodeName){
         // sh """docker login ${registryUrl} --username ${userName} --password ${password}"""
-        sh """cd ${dirContext} && docker build . -t ${registryUrl}/${userName}/${tag}"""
+        sh """cd ${dirContext} && docker build . -t https://${registry}/${userName}/${tag}"""
         }
     }
 
--- docker/steps/push.groovy
+++ docker/steps/push.groovy
@@ -1,12 +1,12 @@
 void call(app_env){
     // String dirContext = app_env.dir_context
     String nodeName = config.nodename ?: "master"
-    String userName = app_env.user
-    String password = app_env.password
-    String registryUrl = app_env.docker_url
+    String userName = app_env.docker_user
+    String password = app_env.docker_password
+    String registry = app_env.docker_registry
     stage("Build Image"){
         node(nodeName){
-        sh """docker login ${registryUrl} --username ${userName} --password ${password}"""
+        sh """docker login https://${registry} --username ${userName} --password ${password}"""
         // sh """cd ${dirContext} && docker build ."""
         }
     }
