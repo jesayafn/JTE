6eac23e107d0a2a462531d17426f638846bbd18e
Jesaya Fernando Napitupulu <52324282+jesayafn@users.noreply.github.com>
2023-02-21 14:05:41 +0700

    add checkout git and fix docker

--- docker/steps/build.groovy
+++ docker/steps/build.groovy
@@ -2,18 +2,12 @@ void call(app_env){
     String nodeName = config.nodename ?: "master"
     String userName = app_env.user
     String password = app_env.password
-    // String credentials = app_env.credentials
-    String registryUrl = app_env.url
-    // String imageTag = app_env.imageTag
-    // if(!userName?.trim() || !password.trim() || !imageTag.trim()){
-    //     println("ABORT! NEED SOME PARAMETER")  
-    // } else{
+    String registryUrl = app_env.docker_url
     node(nodeName){
     stage("Build Image"){
         sh """docker login ${registryUrl} --username ${userName} --password ${password}"""
-        // sh "echo $userName"
-        // println("user is:$credentials")
+        sh """docker build"""
         }
     }
-    // }
+
 }
---
+++ git/steps/checkout.groovy
@@ -0,0 +1,10 @@
+void call(app_env){
+    String url = app_env.git_url
+    String branch = app_env.branch ?: "main"
+	String nodeName = config.nodename ?: "master"
+	node(nodeName){
+        stage("Git Checkout"){
+		//cleanWs()
+		git branch: "${branch}", url: "${git_url}"
+	}
+}
