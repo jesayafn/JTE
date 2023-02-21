87748e8dcd1fcd3a235f4ef5014d586da6e38d69
Jesaya Fernando Napitupulu <52324282+jesayafn@users.noreply.github.com>
2023-02-21 13:17:42 +0700

    add registry url

--- docker/steps/build.groovy
+++ docker/steps/build.groovy
@@ -2,13 +2,14 @@ void call(app_env){
     String nodeName = config.nodename ?: "master"
     String userName = app_env.user
     String password = app_env.password
+    String registryUrl = app_env.url
     // String imageTag = app_env.imageTag
     // if(!userName?.trim() || !password.trim() || !imageTag.trim()){
     //     println("ABORT! NEED SOME PARAMETER")  
     // } else{
     node(nodeName){
     stage("Build Image"){
-        sh """buildah login --username ${userName} --password ${password} --verbose"""
+        sh """docker login --username ${userName} --password ${password} --verbose"""
         // sh "echo $userName"
         // println("user is:$userName")
         }
