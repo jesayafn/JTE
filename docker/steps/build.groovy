4e1aba3bc56cebda83f996d0c1d4f2ae17c52038
Jesaya Fernando Napitupulu <52324282+jesayafn@users.noreply.github.com>
2023-02-21 16:37:00 +0700

    add push (only login) and build

--- docker/steps/build.groovy
+++ docker/steps/build.groovy
@@ -2,12 +2,13 @@ void call(app_env){
     String dirContext = app_env.dir_context
     String nodeName = config.nodename ?: "master"
     String userName = app_env.user
-    String password = app_env.password
+    String tag = app_env.docker_image_tag
+    // String password = app_env.password
     String registryUrl = app_env.docker_url
     stage("Build Image"){
         node(nodeName){
-        sh """docker login ${registryUrl} --username ${userName} --password ${password}"""
-        sh """cd ${dirContext} && docker build ."""
+        // sh """docker login ${registryUrl} --username ${userName} --password ${password}"""
+        sh """cd ${dirContext} && docker build . -t ${registryUrl}/${userName}/${tag}"""
         }
     }
 
---
+++ docker/steps/push.groovy
@@ -0,0 +1,14 @@
+void call(app_env){
+    // String dirContext = app_env.dir_context
+    String nodeName = config.nodename ?: "master"
+    String userName = app_env.user
+    String password = app_env.password
+    String registryUrl = app_env.docker_url
+    stage("Build Image"){
+        node(nodeName){
+        sh """docker login ${registryUrl} --username ${userName} --password ${password}"""
+        // sh """cd ${dirContext} && docker build ."""
+        }
+    }
+
+}
