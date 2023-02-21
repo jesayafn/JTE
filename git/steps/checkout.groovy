e58b240b75bfffd4659790ede29fbc5fa57dde42
Jesaya Fernando Napitupulu <52324282+jesayafn@users.noreply.github.com>
2023-02-21 14:47:33 +0700

    fix

--- git/steps/checkout.groovy
+++ git/steps/checkout.groovy
@@ -1,5 +1,5 @@
 void call(app_env){
-    String git_url = app_env.git_url
+    String gitUrl = app_env.git_url
     String branch = app_env.branch ?: "main"
 	String nodeName = config.nodename ?: "master"
     stage("Git Checkout"){
@@ -15,7 +15,10 @@ void call(app_env){
         //     userRemoteConfigs: [[url: "${url}"]]
         // )
         // println(url)
-        checkout scmGit(branches: [[name: "*/${branch}"]], extensions: [], userRemoteConfigs: [[url: "${git_url}"]])
+        checkout scmGit(
+            branches: [[name: "*/${branch}"]], 
+            extensions: [], 
+            userRemoteConfigs: [[url: "${gitUrl}"]])
 	    }
     }
 }
