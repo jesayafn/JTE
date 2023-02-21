2485a80ceaaad0e6fee78eeddee9fdada43b2db7
Jesaya Fernando Napitupulu <52324282+jesayafn@users.noreply.github.com>
2023-02-21 12:11:37 +0700

    remove if else on docker build

--- docker/steps/build.groovy
+++ docker/steps/build.groovy
@@ -3,13 +3,13 @@ void call(app_env){
     String userName = app_env.user
     String password = app_env.password
     String imageTag = app_env.imageTag
-    if(!userName?.trim() || !password.trim() || !imageTag.trim()){
-        println("ABORT! NEED SOME PARAMETER")  
-    } else{
-        node(nodeName){
-        stage("Build Image"){
-            sh '''buiildah login --username ${app_env.user} --password ${app_env.password --verbose'''
-        }
+    // if(!userName?.trim() || !password.trim() || !imageTag.trim()){
+    //     println("ABORT! NEED SOME PARAMETER")  
+    // } else{
+    node(nodeName){
+    stage("Build Image"){
+        sh '''buiildah login --username ${app_env.user} --password ${app_env.password --verbose'''
     }
-    }
+    // }
+    // }
 }
